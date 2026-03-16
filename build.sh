d43b0ce4698cb360a53e2e6a1c9e2c50f673a9fa
PingouInfini <pingouin@banquise.org>
2026-03-16 23:19:07 +0100

    v2.0.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml build
 
-docker tag pingouinfinihub/hyperv-monitor pingouinfinihub/hyperv-monitor:latest
-docker push pingouinfinihub/hyperv-monitor:latest
+docker tag pingouinfinihub/hyperv-monitor pingouinfinihub/hyperv-monitor:2.0.0
+docker push pingouinfinihub/hyperv-monitor:2.0.0
